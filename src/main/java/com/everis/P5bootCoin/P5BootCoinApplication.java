9a4fedcbc228a0263c1ee059bd40361c28455802
Mauricio06Everis <kpalmall@everis.com>
2021-09-01 11:40:39 -0500

    Update 1

---
+++ src/main/java/com/everis/P5bootCoin/P5BootCoinApplication.java
@@ -0,0 +1,13 @@
+package com.everis.P5bootCoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P5BootCoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P5BootCoinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everis/P5bootCoin/P5BootCoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.P5bootCoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P5BootCoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
